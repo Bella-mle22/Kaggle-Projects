250fef0319773fdf14f60df5c3d8f6063d2240bd
MiaSabre <mia.liu2016@gmail.com>
2016-11-01 12:54:21 -0500

    Update Finding_Misspelled_Words.py

--- Finding_Misspelled_Words.py
+++ Finding_Misspelled_Words.py
@@ -5,6 +5,12 @@
 #uses an if statement that returns True if the current token is not in tokenized_vocabulary and if so, appends the current token to misspelled_words.
 #Print misspelled_words.
 
+#In this code, we're going to explore how to build a basic spell checker. Spell checkers work by comparing each word in a passage of text to a set of correctly spelled words. In this mission, we will learn:
+
+#how to work with plain text files to read in a vocabulary of correctly spelled words,
+#more string methods for working with and processing text data,
+#how to create functions to make the components of our spell checker more reusable.
+
 
 def clean_text(text_string, special_characters):
     cleaned_string = text_string
