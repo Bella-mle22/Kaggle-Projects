e4aee85ca1fb33863ea5f4204832458c3dfa4abf
MiaSabre <mia.liu2016@gmail.com>
2016-11-01 13:09:13 -0500

    Update Finding_Misspelled_Words.py

--- Finding_Misspelled_Words.py
+++ Finding_Misspelled_Words.py
@@ -41,3 +41,46 @@ for ts in tokenized_story:
     if ts not in tokenized_vocabulary:
         misspelled_words.append(ts)
 print(misspelled_words)
+
+
+#Instructions
+#Modify the tokenize() function:
+
+
+
+def tokenize(text_string, special_characters, clean=False):
+    cleaned_story = clean_text(text_string, special_characters)
+    story_tokens = cleaned_story.split(" ")
+    return(story_tokens)
+
+clean_chars = [",", ".", "'", ";", "\n"]
+tokenized_story = []
+tokenized_vocabulary = []
+misspelled_words = []
+#Use an if statement to check if clean is True. If so:
+     #Clean text_string using clean_text and assign the returned string to a variable.
+     #Tokenize this new string variable using the split() method and assign the returned list to a variable.
+     #Return this list.
+# Answer code
+def tokenize(text_string, special_characters, clean=False):
+    # If `clean` is `True`.
+    if clean:
+        cleaned_story = clean_text(text_string, special_characters)
+        story_tokens = cleaned_story.split(" ")
+        return(story_tokens)
+    #Outside the if statement, write the code that's executed if clean is False:
+    #Tokenize text_string using the split() method and assign the returned list to a variable.
+    #Return this list.
+    # If `clean` not equal to `True`, no cleaning.
+    story_tokens = text_string.split(" ")
+    return(story_tokens)
+#Outside the tokenize() function:
+#Use the tokenize() function to clean and tokenize story_string and assign the result to tokenized_story.
+#Use the tokenize() function to tokenize vocabulary and assign the result to tokenized_vocabulary.
+clean_chars = [",", ".", "'", ";", "\n"]
+tokenized_story = tokenize(story_string, clean_chars, True)
+tokenized_vocabulary = tokenize(vocabulary, clean_chars)
+#Finally, loop over in tokenized_story, check if each element is in tokenized_vocabulary, and add to misspelled_words if it isn't.
+for ts in tokenized_story:
+    if ts not in tokenized_vocabulary:
+        misspelled_words.append(ts)
